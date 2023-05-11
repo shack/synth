6bfa180f686c43704504410e4c68b89e5aa62962
Sebastian Hack <hack@cs.uni-saarland.de>
2023-05-11 21:21:01 +0200

    support for operators with different types in signature

--- synth_alt.py
+++ synth_alt.py
@@ -20,31 +20,31 @@ def get_var(ty, args):
 get_var.vars = {}
 
 class Op:
-    def __init__(self, name: str, ty, arity, phi):
-        self.name  = name
-        self.ty    = ty
-        self.arity = arity
-        self.phi   = phi
-        self.comm  = None
+    def __init__(self, name: str, opnd_tys: list, res_ty, phi):
+        self.name     = name
+        self.phi      = phi 
+        self.opnd_tys = opnd_tys
+        self.res_ty   = res_ty
+        self.arity    = len(self.opnd_tys)
+        self.comm     = None
 
     def __str__(self):
         return self.name
 
     def is_commutative(self):
         if self.comm is None:
-            ins = [ get_var(self.ty, (self.name, 'in', 'comm', i)) for i in range(self.arity) ]
-            fs = [ self.phi(a) != self.phi(b) for a, b in comb(perm(ins), 2) ]
+            ins = [ get_var(ty, (self.name, 'in', 'comm', i)) for i, ty in enumerate(self.opnd_tys) ]
+            fs = [ self.phi(a) != self.phi(b) for a, b in comb(perm(ins), 2) ] 
             s = Solver()
             s.add(Or(fs))
             self.comm = s.check() == unsat
         return self.comm
 
 class Prg:
-    def __init__(self, ty, input_names, insns, outputs):
+    def __init__(self, input_names, insns, outputs):
         """Creates a program.
 
         Attributes:
-        ty: type of the program variables
         input_names: list of names of the inputs
         insns: List of instructions.
             This is a list of pairs where each pair consists
@@ -52,7 +52,6 @@ class Prg:
             indicates the variable number of the operand.
         outputs: List of variable numbers that constitute the output.
         """
-        self.ty = ty
         self.input_names = input_names
         self.insns = insns
         self.outputs = outputs
@@ -73,42 +72,41 @@ class Prg:
         return res + f'\nreturn({jv(self.outputs)})'
 
 class Synth:
-    def __init__(self, n_insns, ty, input_names, funcs: list[Op], ops: list[Op]):
-        self.ty = ty
+    def __init__(self, n_insns, input_names, funcs: list[Op], ops: list[Op]):
         self.input_names = input_names
         self.n_inputs = len(input_names)
         self.n_insns = n_insns
         self.funcs = funcs
         self.ops = ops
 
-        # get the maximum arity of the operators we have
-        assert(len(ops) > 0)
-        self.max_arity = max(map(lambda o: o.arity, ops))
-
-        # construct a list of arities per instruction
-        # inputs have arity 0, output has arity of number of functions we synthesize
-        self.arities = ([ 0 ] * self.n_inputs) + ([ self.max_arity ] * n_insns) + [ len(funcs) ]
-        self.length = len(self.arities)
-
-        # all specifications need to agree on number of inputs
-        assert(len(funcs) > 0)
-        arity = funcs[0].arity
+        # get types of input operands. 
+        # all functions need to agree on this.
+        assert len(funcs) > 0
+        self.in_tys = funcs[0].opnd_tys
         for s in funcs[1:]:
-            assert arity == s.arity
+            assert self.in_tys == s.opnd_tys
+        self.length = len(self.in_tys) + n_insns + 1
+
+        max_arity = max(map(lambda op: op.arity, ops))
+        self.arities = [ 0 ] * self.n_inputs + [ max_arity ] * n_insns + [ len(funcs) ]
+        self.out_tys = [ op.res_ty for op in funcs ]
+
+    def var_insn_op(self, insn):
+        return get_var(Int, ('insn', insn, 'op'))
 
     def var_insn_opnds(self, insn):
         for opnd in range(self.arities[insn]):
             yield get_var(Int, ('insn', insn, 'opnd', opnd))
 
-    def var_insn_opnds_val(self, insn, instance):
-        for opnd in range(self.arities[insn]):
-            yield get_var(self.ty, ('insn', insn, 'opnd', opnd, 'val', instance))
+    def var_insn_opnds_val(self, insn, tys, instance):
+        for opnd, ty in enumerate(tys):
+            yield get_var(ty, ('insn', insn, 'opnd', opnd, ty.__name__, instance))
 
-    def var_insn_res(self, insn, instance):
-        return get_var(self.ty, ('insn', insn, 'res', instance))
+    def var_insn_res(self, insn, ty, instance):
+        return get_var(ty, ('insn', insn, 'res', ty.__name__, instance))
 
-    def var_insn_op(self, insn):
-        return get_var(Int, ('insn', insn, 'op'))
+    def var_input_res(self, insn, instance):
+        return self.var_insn_res(insn, self.in_tys[insn], instance)
 
     def add_constr_wfp(self, solver: Solver):
         # acyclic: line numbers of uses are lower than line number of definition
@@ -119,10 +117,10 @@ class Synth:
                 solver.add(v < insn)
 
         # add constraints that says that each produced value is used
-        for prod in range(self.n_insns, self.length):
-            opnds = [ prod == v for cons in range(prod + 1, self.length) for v in self.var_insn_opnds(cons) ]
-            if len(opnds) > 1:
-                solver.add(Or(opnds))
+        # for prod in range(self.n_insns, self.length):
+        #     opnds = [ prod == v for cons in range(prod + 1, self.length) for v in self.var_insn_opnds(cons) ]
+        #     if len(opnds) > 1:
+        #         solver.add(Or(opnds))
 
         # for all instructions that get an op
         for insn in range(self.n_inputs, self.length - 1):
@@ -140,8 +138,20 @@ class Synth:
                     c = [ l < u for l, u in zip(opnds[:op.arity - 1], opnds[1:]) ]
                     solver.add(Implies(op_var == op_id, And(c)))
 
+    def out_insn(self):
+        return self.length - 1
+
     def is_op_insn(self, insn):
         return insn >= self.n_inputs and insn < self.length - 1
+
+    def add_constr_conn(self, solver, insn, tys, instance):
+        for l, v in zip(self.var_insn_opnds(insn), \
+                        self.var_insn_opnds_val(insn, tys, instance)):
+            # for other each instruction preceding it
+            for other in range(insn):
+                # print(v.sort(), l.sort())
+                r = self.var_insn_res(other, Bool, instance)
+                solver.add(Implies(l == other, v == r))
 
     def add_constr_instance(self, solver, instance):
         # for all instructions that get an op
@@ -149,25 +159,23 @@ class Synth:
             # add constraints to select the proper operation
             op_var = self.var_insn_op(insn)
             for op_id, op in enumerate(self.ops):
-                res = self.var_insn_res(insn, instance)
-                opnds = list(self.var_insn_opnds_val(insn, instance))
-                eq = res == op.phi(opnds[:op.arity])
+                res = self.var_insn_res(insn, op.res_ty, instance)
+                opnds = list(self.var_insn_opnds_val(insn, op.opnd_tys, instance))
+                eq = res == op.phi(opnds)
                 solver.add(Implies(op_var == op_id, eq))
-
-        for insn in range(self.length):
             # connect values of operands to values of corresponding results
-            for l, v in zip(self.var_insn_opnds(insn), self.var_insn_opnds_val(insn, instance)):
-                # for other each instruction preceding it
-                for other in range(insn):
-                    r = self.var_insn_res(other, instance)
-                    solver.add(Implies(l == other, v == r))
+            for op in self.ops:
+                self.add_constr_conn(solver, insn, op.opnd_tys, instance)    
+
+        # add connection constraints for output instruction
+        self.add_constr_conn(solver, self.out_insn(), self.out_tys, instance)
 
     def add_constr_input_values(self, solver, instance, counter_example):
         # add input value constraints
         assert len(counter_example) == self.n_inputs
         for inp, val in zip(range(self.n_inputs), counter_example):
             if not val is None:
-                res = self.var_insn_res(inp, instance)
+                res = self.var_input_res(inp, instance)
                 solver.add(res == val)
 
     def add_constr_sol_for_verif(self, solver, model):
@@ -179,20 +187,20 @@ class Synth:
                 solver.add(opnd == model[opnd])
 
     def add_constr_spec(self, solver, instance, f=lambda x: x):
-        out_insn = self.length - 1
         # all result variables of the inputs
-        input_vals = [ self.var_insn_res(i, instance) for i in range(self.n_inputs) ]
+        input_vals = [ self.var_input_res(i, instance) \
+                       for i in range(self.n_inputs) ]
         # the operand value variables of the output instruction
-        out_opnds = self.var_insn_opnds_val(out_insn, instance)
+        out_opnds = self.var_insn_opnds_val(self.out_insn(), self.out_tys, instance)
         # constraints that express the specifications
         spec = [o == s.phi(input_vals) for o, s in zip(out_opnds, self.funcs)]
         solver.add(f(And(spec)))
 
     def sample(self):
         s = Solver()
-        ins = [ get_var(self.ty, ('sample', 'in', i)) for i in range(self.n_inputs) ]
+        ins = [ get_var(self.in_tys[i], ('sample', 'in', i)) for i in range(self.n_inputs) ]
         for i, func in enumerate(self.funcs):
-            res = get_var(self.ty, ('sample', 'out', i))
+            res = get_var(func.res_ty, ('sample', 'out', i))
             s.add(res == func.phi(ins))
         s.check()
         m = s.model()
@@ -251,7 +259,7 @@ class Synth:
                 if verif.check() == sat:
                     # there is a counterexample, reiterate
                     m = verif.model()
-                    var = lambda inp : m[self.var_insn_res(inp, 'verif')]
+                    var = lambda inp : m[self.var_input_res(inp, 'verif')]
                     counter_example = [ var(inp) for inp in range(self.n_inputs) ]
                     verif.pop()
                     i += 1
@@ -265,17 +273,16 @@ class Synth:
                         insns += [ (op, opnds) ]
                     out_idx = self.length - 1
                     outputs = [ m[res].as_long() for res in self.var_insn_opnds(out_idx) ]
-                    return Prg(self.ty, self.input_names, insns, outputs)
+                    return Prg(self.input_names, insns, outputs)
             else:
                 d('synthesis failed')
                 return None
 
-def synth(length, ty, input_names, specs, ops, debug=False):
+def synth(length, input_names, specs, ops, debug=False):
     """Synthesize a program that implements a given specification.
 
     Args:
-        length: Maximum length of the program.
-        ty: The type of the program's variables.
+        length: Length of the program.
         input_names (list(str)): A list of names of the input variables
         specs (list(Op)): List of specifications of functions to synthesize
         ops (list(Op)): List of available operators that can be used for instructions.
@@ -283,9 +290,9 @@ def synth(length, ty, input_names, specs, ops, debug=False):
     Returns:
         A program (Prg) if synthesis was successful or None otherwise.
     """
-    return Synth(length, ty, input_names, specs, ops)(debug)
+    return Synth(length, input_names, specs, ops)(debug)
 
-def synth_smallest(max_length, ty, input_names, specs, ops, debug=False):
+def synth_smallest(max_length, input_names, specs, ops, debug=False):
     """Synthesize the smallest program that implements a given specification.
 
     Use like synth except for max_length which gives an upper bound on
@@ -293,30 +300,36 @@ def synth_smallest(max_length, ty, input_names, specs, ops, debug=False):
     from 1 to max_length + 1 and returns the first (smallest) program found.
     """
     for sz in range(0, max_length + 1):
-        if prg := synth(sz, ty, input_names, specs, ops, debug):
+        if prg := synth(sz, input_names, specs, ops, debug):
             return prg
     return None
 
-true0  = Op('true0', Bool, 0, lambda ins: True)
-false0 = Op('false0',Bool, 0, lambda ins: False)
-id1    = Op('id1',   Bool, 1, lambda ins: ins[0])
-not1   = Op('not',   Bool, 1, lambda ins: Not(ins[0]))         #7404
-nand2  = Op('nand2', Bool, 2, lambda ins: Not(And(ins)))       #7400
-nor2   = Op('nor2',  Bool, 2, lambda ins: Not(Or(ins)))        #7402
-and2   = Op('and2',  Bool, 2, And)                             #7408
-or2    = Op('or2',   Bool, 2, Or)                              #7432
-xor2   = Op('xor2',  Bool, 2, lambda ins: Xor(ins[0], ins[1])) #7486
+Bool1 = [ Bool ]
+Bool2 = [ Bool ] * 2
+Bool3 = [ Bool ] * 3
+Bool4 = [ Bool ] * 4
 
-nand3 = Op('nand3', Bool, 3, lambda ins: Not(And(ins)))       #7410
-nor3  = Op('nor3',  Bool, 3, lambda ins: Not(Or(ins)))        #7427
-and3  = Op('and3',  Bool, 3, And)                             #7411
+true0  = Op('true0',  []   , Bool, lambda ins: True)
+false0 = Op('false0', []   , Bool, lambda ins: False)
+id1    = Op('id1',    Bool1, Bool, lambda ins: ins[0])
 
-nand4 = Op('nand4', Bool, 4, lambda ins: Not(And(ins)))       #7420
-and4  = Op('and4',  Bool, 4, And)                             #7421
-nor4  = Op('nor4',  Bool, 4, lambda ins: Not(Or(ins)))        #7429
-
-mux2  = Op('mux2',  Bool, 2, lambda i: Or(And(i[0], i[1]), And(Not(i[0]), i[2])))
-eq2   = Op('eq2',   Bool, 2, lambda i: i[0] == i[1])
+not1  = Op('not',     Bool2, Bool, lambda ins: Not(ins[0]))         #7404
+nand2 = Op('nand2',   Bool2, Bool, lambda ins: Not(And(ins)))       #7400
+nor2  = Op('nor2',    Bool2, Bool, lambda ins: Not(Or(ins)))        #7402
+and2  = Op('and2',    Bool2, Bool, And)                             #7408
+or2   = Op('or2',     Bool2, Bool, Or)                              #7432
+xor2  = Op('xor2',    Bool2, Bool, lambda ins: Xor(ins[0], ins[1])) #7486
+        
+nand3 = Op('nand3',   Bool3, Bool, lambda ins: Not(And(ins)))       #7410
+nor3  = Op('nor3',    Bool3, Bool, lambda ins: Not(Or(ins)))        #7427
+and3  = Op('and3',    Bool3, Bool, And)                             #7411
+        
+nand4 = Op('nand4',   Bool4, Bool, lambda ins: Not(And(ins)))       #7420
+and4  = Op('and4',    Bool4, Bool, And)                             #7421
+nor4  = Op('nor4',    Bool4, Bool, lambda ins: Not(Or(ins)))        #7429
+        
+mux2  = Op('mux2',    Bool2, Bool, lambda i: Or(And(i[0], i[1]), And(Not(i[0]), i[2])))
+eq2   = Op('eq2',     Bool2, Bool, lambda i: i[0] == i[1])
 
 def create_random_formula(inputs, size, ops, seed=0x5aab199e):
     random.seed(a=seed, version=2)
@@ -354,9 +367,9 @@ def create_random_dnf(inputs, seed=0x5aab199e):
 def random_test(n_vars, size, create_formula, debug=0):
     params = [ get_var(Bool, ('var', i)) for i in range(n_vars) ]
     ops  = [ and2, or2, xor2, not1 ]
-    spec = Op('rand', Bool, vars, create_formula)
+    spec = Op('rand', [ Bool ] * n_vars, Bool, create_formula)
     print(create_formula(params))
-    prg  = synth_smallest(size, Bool, [ f'v{i}' for i in range(n_vars)], [spec], ops, debug)
+    prg  = synth_smallest(size, [ f'v{i}' for i in range(n_vars)], [spec], ops, debug)
     print(prg)
 
 def test_rand(size=40, n_vars=10, debug=0):
@@ -369,38 +382,38 @@ def test_rand_dnf(size=40, n_vars=10, debug=0):
     random_test(n_vars, size, f, debug)
 
 def test_and(debug=0):
-    spec = Op('and', Bool, 2, And)
+    spec = Op('and', Bool2, Bool, And)
     print('and:')
-    prg = synth_smallest(1, Bool, [ 'a', 'b'], [spec], [spec], debug)
-    print(prg)
-
-def test_mux(debug=0):
-    spec = Op('mux2', Bool, 3, lambda i: Or(And(i[0], i[1]), And(Not(i[0]), i[2])))
-    ops  = [ and2, xor2 ]
-    print('mux:')
-    prg = synth_smallest(3, Bool, [ 's', 'x', 'y' ], [ spec ], ops, debug)
+    prg = synth_smallest(1, [ 'a', 'b'], [spec], [spec], debug)
     print(prg)
 
 def test_xor(debug=0):
-    spec = Op('xor2', Bool, 2, lambda i: Xor(i[0], i[1]))
+    spec = Op('xor2', Bool2, Bool, lambda i: Xor(i[0], i[1]))
     ops  = [ and2, nand2, or2, nor2 ]
     print('xor:')
-    prg = synth_smallest(10, Bool, [ 'x', 'y' ], [ spec ], ops, debug)
+    prg = synth_smallest(10, [ 'x', 'y' ], [ spec ], ops, debug)
+    print(prg)
+
+def test_mux(debug=0):
+    spec = Op('mux2', Bool3, Bool, lambda i: Or(And(i[0], i[1]), And(Not(i[0]), i[2])))
+    ops  = [ and2, xor2 ]
+    print('mux:')
+    prg = synth_smallest(3, [ 's', 'x', 'y' ], [ spec ], ops, debug)
     print(prg)
 
 def test_zero(debug=0):
-    spec = Op('zero', Bool, 8, lambda i: Not(Or(i)))
+    spec = Op('zero', [ Bool ] * 8, Bool, lambda i: Not(Or(i)))
     ops  = [ and2, nand2, or2, nor2, nand3, nor3, nand4, nor4 ]
-    print('zero:')
-    prg = synth_smallest(10, Bool, [ f'x{i}' for i in range(8) ], [ spec ], ops, debug)
+    print('one byte all zero test:')
+    prg = synth_smallest(10, [ f'x{i}' for i in range(8) ], [ spec ], ops, debug)
     print(prg)
 
 def test_add(debug=0):
-    cy  = Op('cy',  Bool, 3, lambda i: Or(And(i[0], i[1]), And(i[1], i[2]), And(i[0], i[2])))
-    add = Op('add', Bool, 3, lambda i: Xor(i[0], Xor(i[1], i[2])))
+    cy  = Op('cy',  Bool3, Bool, lambda i: Or(And(i[0], i[1]), And(i[1], i[2]), And(i[0], i[2])))
+    add = Op('add', Bool3, Bool, lambda i: Xor(i[0], Xor(i[1], i[2])))
     ops = [ nand2, nor2, and2, or2, xor2 ]
     print('1-bit full adder:')
-    prg = synth_smallest(10, Bool, [ 'x', 'y', 'c' ], [ add, cy ], ops, debug)
+    prg = synth_smallest(10, [ 'x', 'y', 'c' ], [ add, cy ], ops, debug)
     print(prg)
 
 def test_identity(debug=0):
