f5efba5a4279058ba6a9497191fdfb9c083f0a94
Sebastian Hack <hack@cs.uni-saarland.de>
2024-06-02 14:17:54 +0200

    fixed bug in max_const in the output instruction

--- synth_n.py
+++ synth_n.py
@@ -230,7 +230,8 @@ class SynthN:
         # Add a constraint for the maximum amount of constants if specified.
         # The output instruction is exempt because we need to be able
         # to synthesize constant outputs correctly.
-        max_const_ran = range(self.n_inputs, self.length - 1)
+        max_const_ran = range(self.n_inputs, self.length)
+        # max_const_ran = range(self.n_inputs, self.length - 1)
         if not max_const is None and len(max_const_ran) > 0:
             solver.add(AtMost(*[ v for insn in max_const_ran \
                         for v in self.var_insn_opnds_is_const(insn)], max_const))
