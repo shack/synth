bd4c5301c48e0fed5f66280a14b7c641590e024e
Nicolas Edelmann <niggelgame@gmail.com>
2024-11-07 13:06:50 -0800

    remove old downscaling implementation

--- synth/synth_constants_fa_stage.py
+++
@@ -1,7 +0,0 @@
-from synth.cegis import Spec
-
-import synth_constants_cegis_stage
-
-def synth(spec: Spec, ops, iter_range, n_samples=1, **args):
-    synth.synth_constants_cegis_stage.use_cegis = False
-    return synth.synth_constants_cegis_stage.synth(spec, ops, iter_range, n_samples, **args)
